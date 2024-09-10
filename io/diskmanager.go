58157340410503af767c7556074332ac94c3cdac
Ngina <ngina@users.noreply.github.com>
2024-09-10 16:24:33 -0400

    Create DiskManager interface for managing disk operations
    - Add DiskManager interface to abstract disk-related operations
    - Define writePage/readPage methods

---
+++ io/diskmanager.go
@@ -0,0 +1,47 @@
+package io
+
+const (
+	// MaxPageSize is the max page size of the db is 4K bytes,
+	// which is the typical OS page size.
+	MaxPageSize = 4 * 1024
+)
+
+type DiskManager interface {
+	writePage(pageNumber uint64, contents []byte) error
+	readPage(pageNumber uint64, p []byte) error
+}
+
+type DefaultDiskManager struct{}
+
+func (d DefaultDiskManager) writePage(pageNumber uint64, contentsToWrite []byte) error {
+	return nil
+}
+
+func (d DefaultDiskManager) readPage(pageNumber uint64, buf []byte) error {
+	return nil
+}
+
+// toFile saves buffer data to disk.
+// Creates file if it does not exist, or truncates the exisiting one before
+// writing the content.
+//
+// This function is not atomic and concurrent readers may get half updated data.
+// func (d DefaultDiskManager) toFile(path string, buf []byte) error {
+// 	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
+
+// 	_, err = f.Write(buf)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Explicitly flush file buffer content to disk.
+// 	return f.Sync()
+// }
+
+// func (d DefaultDiskManager) fromFile(path string, buf []byte) error {
+// 	return nil
+// }
